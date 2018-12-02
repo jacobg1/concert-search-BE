ff7d667eba147ad3e7d9fb4e314f6511ad4cc8aa
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-02 12:47:59 -0500

    add random song by ID route

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const port = process.env.PORT || 3000
 
 const getMeta = require('./routes/getMeta'),
       concertById = require('./routes/concertById'),
-      randomSong = require('./routes/randomSong')
+      randomSong = require('./routes/randomSong'),
+      randomSongById = require('./routes/randomSongById')
 
-
-// app.use(bodyParser.json())
-
-app.use( getMeta, concertById, randomSong )
+app.use( getMeta, concertById, randomSong, randomSongById )
 
 app.listen( port, () => console.log(`Listening on port ${ port }!`))
