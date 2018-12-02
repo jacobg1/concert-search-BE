f1a86b9638753cc1310fd513642c2989e1fade14
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-01 20:33:21 -0500

    route to get songs by identifier

---
+++ routes/concertById.js
@@ -0,0 +1,54 @@
+const express = require('express'),
+    router = express.Router(),
+    axios = require('axios')
+
+router.get('/concert/:id', function(req, res, next) {
+    
+    // take in id parameter
+    let concertId = req.params.id
+
+    // build search url
+    let url = 'https://archive.org/metadata/' + concertId
+
+    // make api call
+    axios({
+        method: 'GET',
+        url: url,
+        dataType: 'jsonp'
+    })
+    .then((response) => {
+
+        // build playback url base
+        let { d1, dir } = response.data
+        let base = 'https://' + d1 + dir + '/'
+
+        // filter results for correct audio format
+        let mp3Tracks = response.data.files.filter(function (song) {
+            return song.format === 'VBR MP3'
+        })
+
+        // add the built play url as an object key
+        mp3Tracks.forEach(track => {
+            track.playUrl = base + track.name
+        });
+
+        // build response object
+        let concertObject = {}
+
+        // add metadata
+        concertObject.metaData = response.data.metadata
+        
+        // add track list
+        concertObject.trackList = mp3Tracks
+        
+        // send result to front end
+        res.send(concertObject)
+
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+})
+
+module.exports = router
