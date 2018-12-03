1d2858e436946e067bcfffd82cdcda2dc1a6232f
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-03 00:38:15 -0500

    allowing crossdomain requests

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const getMeta = require('./routes/getMeta'),
       randomSong = require('./routes/randomSong'),
       randomSongById = require('./routes/randomSongById')
 
-app.use( getMeta, concertById, randomSong, randomSongById )
+
+// CORS middleware
+var allowCrossDomain = function (req, res, next) {
+
+    // Allowing all for now, change in prod
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain, getMeta, concertById, randomSong, randomSongById )
 
 app.listen( port, () => console.log(`Listening on port ${ port }!`))
