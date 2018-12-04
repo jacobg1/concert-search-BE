4dc767cad7972d47fcdaa938cc6da8d52f8930dd
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-03 19:26:16 -0500

    filter by year instead of adding to search params

--- routes/getMeta.js
+++ routes/getMeta.js
@@ -12,19 +12,20 @@ const express = require('express'),
 
 router.get('/meta/:artistName/:artistYear?', function (req, res, next) {
     
-    // take in and format search params, year is optional so
-    // only add year if it exists as part of search
+    // take in and format search params, note that
+    // year is never added to the api call, however it will be used to filter 
+    // the results by year
     let artist = req.params.artistName.replace(/ /g, '+')
     let year = req.params.artistYear || ''
     let searchParams = artist
-    if (year) searchParams += '+AND+year%3A' + year
+    // if (year) searchParams += '+AND+year%3A' + year
 
     // build meta search url, this will return ids
     // for use in metadata search
     let url = 'https://archive.org/advancedsearch.php?q=creator%3A' + searchParams + '&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=title&&fl%5B%5D=description&fl%5B%5D=year&sort%5B%5D=year+asc&sort%5B%5D=&sort%5B%5D=&rows=1000&page=&output=json'
     
     // check if exists in redis storage
-    client.exists( searchParams, function ( err, reply ) {
+    client.exists( artist, function ( err, reply ) {
 
         // exists() returns 1 if exists
         if ( reply === 1 ) {
@@ -32,13 +33,28 @@ router.get('/meta/:artistName/:artistYear?', function (req, res, next) {
             console.log('fetching from redis store')
 
             // get result from redis instead of making api call
-            client.get( searchParams, function ( error, result ) {
+            client.get( artist, function ( error, result ) {
+
+                // decode result from JSON so that it can be manipulated
+                let parseResult = JSON.parse( result )
 
                 // handle errors
                 if ( error ) throw error
 
-                    // send result
-                    res.send(JSON.parse( result ))
+                    // if year exists in params, filter by year param
+                    if( year ) {
+
+                        let filterByYear = parseResult.filter(( item ) => {
+                            return item.year === year
+                        })
+
+                        // send year filtered result
+                        res.send( filterByYear )
+
+                    } else {
+                        // send result
+                        res.send( parseResult )
+                    }
             })
 
         } else {
@@ -59,10 +75,23 @@ router.get('/meta/:artistName/:artistYear?', function (req, res, next) {
                 })
 
                 // set hash in redis storage
-                client.set(searchParams, JSON.stringify( concertsOnly ))
-                
-                // send result
-                res.send( concertsOnly )
+                client.set(artist, JSON.stringify(concertsOnly))
+
+                // if year exists in params, filter by year param
+                if ( year ) {
+                    
+                    let filterByYear = concertsOnly.filter(( item ) => {
+                        return item.year === year
+                    })
+
+                    // send year filtered result
+                    res.send( filterByYear )
+
+                } else {
+
+                    // send result
+                    res.send( concertsOnly )
+                }
             })
             .catch(( error ) => {
                 console.log( error )
