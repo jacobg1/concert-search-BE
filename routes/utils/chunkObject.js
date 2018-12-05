575922ee6919989ee7d800534e39262cf136b92f
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-04 19:40:04 -0500

    add hasMore key value pair

--- routes/utils/chunkObject.js
+++ routes/utils/chunkObject.js
@@ -4,12 +4,25 @@ function chunkObject( myObject, chunk_size ) {
     let index = 0
     let objectLength = myObject.length
     let resultArray = []
-
+   
     for (index = 0; index < objectLength; index += chunk_size) {
 
         myChunk = myObject.slice( index, index + chunk_size )
 
+        myChunk = {...myChunk}
+        
         resultArray.push( myChunk )
+       
+    }
+    
+    // add has more key value pair if it isn't the last chunk
+    for(let x = 0; x < resultArray.length; x++) {
+
+        if(x !== ( resultArray.length - 1 )) {
+            resultArray[x].hasMore = true
+        } else {
+            resultArray[x].hasMore = false
+        }
     }
 
     return resultArray
