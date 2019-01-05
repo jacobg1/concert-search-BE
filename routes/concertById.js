5259031eccfaabfccd4f57c77af462664d5660e8
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-05 16:35:26 -0500

    removing caching from route

--- routes/concertById.js
+++ routes/concertById.js
@@ -1,13 +1,7 @@
 const express = require('express'),
     router = express.Router(),
     axios = require('axios'),
-    redis = require('redis'),
-    client = redis.createClient()
-
-    // handle redis errors
-    client.on('error', function ( err ) {
-        console.log('Error ' + err);
-    });
+    redis = require('redis')
 
 router.get('/concert/:id', function( req, res, next ) {
     
@@ -17,71 +11,47 @@ router.get('/concert/:id', function( req, res, next ) {
     // build search url
     let url = 'https://archive.org/metadata/' + concertId
 
-    // check if exists in redis storage
-    client.exists( concertId, function ( err, reply ) {
+    // make api call
+    axios({
+        method: 'GET',
+        url: url,
+        dataType: 'jsonp'
+    })
+    .then(( response ) => {
 
-        // exists() returns 1 if exists
-        if ( reply === 1) {
+        // build playback url base
+        let { d1, dir } = response.data
+        let base = 'https://' + d1 + dir + '/'
 
-            console.log('fetching from redis store')
+        // filter results for correct audio format
+        let mp3Tracks = response.data.files.filter(function (song) {
+            return song.format === 'VBR MP3'
+        })
 
-            // get result from redis instead of making api call
-            client.get( concertId, function ( error, result ) {
+        // add the built play url as an object key
+        mp3Tracks.forEach( track => {
+            track.playUrl = base + track.name.replace(/ /g, '%20')
+        });
 
-                // handle errors
-                if ( error ) throw error
+        // build response object
+        let concertObject = {}
 
-                // send result
-                res.send(JSON.parse( result ))
-            })
+        // add metadata
+        concertObject.metaData = response.data.metadata
 
-        } else {
+        // add track list
+        concertObject.trackList = mp3Tracks
 
-            console.log('fetching from api')
+        // save concert object in redis cache
+        // client.set( concertId, JSON.stringify( concertObject ))
 
-            // make api call
-            axios({
-                method: 'GET',
-                url: url,
-                dataType: 'jsonp'
-            })
-            .then(( response ) => {
+        // send result to front end
+        res.send( concertObject )
 
-                // build playback url base
-                let { d1, dir } = response.data
-                let base = 'https://' + d1 + dir + '/'
-
-                // filter results for correct audio format
-                let mp3Tracks = response.data.files.filter(function (song) {
-                    return song.format === 'VBR MP3'
-                })
-
-                // add the built play url as an object key
-                mp3Tracks.forEach( track => {
-                    track.playUrl = base + track.name.replace(/ /g, '%20')
-                });
-
-                // build response object
-                let concertObject = {}
-
-                // add metadata
-                concertObject.metaData = response.data.metadata
-
-                // add track list
-                concertObject.trackList = mp3Tracks
-
-                // save concert object in redis cache
-                // client.set( concertId, JSON.stringify( concertObject ))
-
-                // send result to front end
-                res.send( concertObject )
-
-            })
-            .catch(( error ) => {
-                console.log( error )
-            })
-        }
-    })        
+    })
+    .catch(( error ) => {
+        console.log( error )
+    })
 })
 
 module.exports = router
